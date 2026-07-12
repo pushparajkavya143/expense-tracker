50b9a3569697665bc5ccf353682c976cdb435823
pushparajkavya143 <pushparajkavya1431@gmail.com>
2026-07-12 16:35:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ def index():
     return redirect(url_for('dashboard'))
 
 @app.route('/dashboard')
-@login_required
+#@login_required
 def dashboard():
     transactions = Transaction.query.filter_by(user_id=current_user.id).all()
     return render_template("dashboard.html", transactions=transactions)
 
 # --- Transaction சேர்ப்பதற்கான ரூட் ---
 @app.route('/transaction/add', methods=['GET', 'POST'])
-@login_required
+#@login_required
 def add_transaction():
     if request.method == 'POST':
         try:
